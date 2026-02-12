2f1e1014dad9c4f8b3ea95fdfcdee5db25ff3503
Shikhar9960 <shikharjais5@gmail.com>
2026-02-12 18:19:49 +0530

    express1

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = 4000
+
+app.get("/",(req,res)=>{
+    res.send("this is home page")
+})
+
+app.get("/login",(req,res)=>{
+    res.send("this is login page")
+})
+
+app.get("/signup",(req,res)=>{
+    res.send("this is a sign up page")
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log(`my app is listening on port ${PORT}`)
+})
